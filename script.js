f78d3d894d35ed1cd780447a1035a3222d1437f4
Nick Jackson <nickdjackson@msn.com>
2023-01-12 13:56:32 -0500

    addded logic

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-import bot 
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
+
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = "";
+
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300);
+}
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.chartAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+function generateUnique() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueID) {
+  return `
+    <div class='wrapper ${isAi && "ai"}>
+    <div class='chat'>
+    <div class='profile>
+    <img
+    src="${isAi ? bot : user}"
+    />
+    </div>
+    <div class="message"></div>
+    </div>
+    </div>
+    
+    `;
+}
